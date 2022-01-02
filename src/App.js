ee12bdd2584b4c85c99750d524a0f70d250d06af
rfmdiez <rfmdiez@gmail.com>
2022-01-02 01:40:57 -0500

    App React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import Message from './Message';
+
+const Description = () => {
+  return <p>Esta es la descripion del curso</p>
+}
+
+const App = () =>{
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hola Mundooo
+      <Message color='red' msg='Estamos trabajando ' />
+      <Message color='green' msg='En un curso' />
+      <Message color='yellow' msg='De React' />
+      <Description />
     </div>
   );
 }
---
+++ src/Message.js
@@ -0,0 +1,6 @@
+const Message = (props) =>{
+    console.log(props)
+    return <h1 style={{color : props.color}}>{props.msg}</h1>
+}
+
+export default Message
